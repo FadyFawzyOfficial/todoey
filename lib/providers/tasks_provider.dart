4bfd719538a3fdcb23454561c42e125c21df2a7b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-04-23 13:21:05 +0200

    8. Make the tasks List in TasksProvider Private & not Accessible

--- lib/providers/tasks_provider.dart
+++ lib/providers/tasks_provider.dart
@@ -1,18 +1,22 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/task.dart';
 
 class TasksProvider extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(title: 'Buy milk'),
     Task(title: 'Buy eggs'),
     Task(title: 'Buy bread'),
   ];
 
-  int get tasksCount => tasks.length;
+  UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
+
+  int get tasksCount => _tasks.length;
 
   void addTask(String taskTitle) {
-    tasks.add(Task(title: taskTitle));
+    _tasks.add(Task(title: taskTitle));
     notifyListeners();
   }
 }
